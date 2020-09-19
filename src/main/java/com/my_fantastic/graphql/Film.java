255e256e4d8d4f0c86d6c46a4ee811c563224fb8
Leandro Alexandrino <alexandrinowd@gmail.com>
2020-09-19 16:18:17 -0300

    initial commit

---
+++ src/main/java/com/my_fantastic/graphql/Film.java
@@ -0,0 +1,44 @@
+package com.my_fantastic.graphql;
+
+import java.time.LocalDate;
+
+public class Film {
+
+    private String title;
+    private Integer episodeId;
+    private String director;
+    private LocalDate releaseDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getEpisodeId() {
+        return episodeId;
+    }
+
+    public void setEpisodeId(Integer episodeId) {
+        this.episodeId = episodeId;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+}
---
+++ src/main/java/com/my_fantastic/graphql/LightSaber.java
@@ -0,0 +1,5 @@
+package com.my_fantastic.graphql;
+
+public enum LightSaber {
+    RED, BLUE, GREEN, PURPLE
+}
